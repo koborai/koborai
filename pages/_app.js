548added3606c6a6c062eda853a9ad670e0c84df
koborai <koboraibusiness@gmail.com>
2024-12-01 21:44:26 +0800

    Menghapus file _app.js dan index.html.bak

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-// pages/_app.js
-
-import '../styles/globals.css'; // Mengimpor CSS global
-import '../public/styles.css';  // Mengimpor CSS tambahan jika diperlukan
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
